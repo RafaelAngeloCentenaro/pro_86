fece6d3dc8577c96e3b5770679f8db6caeff8192
RafaelAngeloCentenaro <105504416+RafaelAngeloCentenaro@users.noreply.github.com>
2023-02-03 10:29:51 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# pro_86
+# PRO-C85-boilerplate
+
---
+++ config.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBqZtUmIUm9WWE7eWtwNeAPxww4FYZoF3o",
+  authDomain: "pro85-1e453.firebaseapp.com",
+  projectId: "pro85-1e453",
+  storageBucket: "pro85-1e453.appspot.com",
+  messagingSenderId: "204708871795",
+  appId: "1:204708871795:web:d728b6f83310886424b406"
+};
+
